26d8e9970471c9f0521e4201a82ffa08052de738
Kez <100411374+21kezia@users.noreply.github.com>
2022-03-16 15:48:04 +0700

    Create tilesfactory.h

---
+++ tilesfactory.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "tiles.h"
+#include "TNT.h"
+#include "rocket.h"
+
+using namespace std;
+
+class tilesFactory
+{
+public:
+	tiles* createTiles(string type, int X, int Y);
+};
