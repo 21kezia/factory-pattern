b3e3b2380a5929ef99711912174a647252784ac0
Kez <100411374+21kezia@users.noreply.github.com>
2022-03-16 15:23:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include "TNT.h"
 #include "tiles.h";
 #include <iostream>;
+
 using namespace std;
 
-int main() {
+int main() 
+{
 	board a;
 	a.getTiles(1, 1)->explode();
 	a.getTiles(2, 2)->explode();
